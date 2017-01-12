58982ef456c394846cc4ec942c788b2fa96f7002
flaw600 <sikand@gmail.com>
2017-01-11 23:22:15 -0600

    Updated watchfile

--- watchfile.js
+++ watchfile.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 
+console.log(fs.existsSync("./watchlist.json"));
 if (!fs.existsSync("./watchlist.json")) {
     fs.writeFile("./watchlist.json", "{}", {flag: 'wx'},  (err) => {
         if (err) console.error(err);
